de6922c8f9a6c7b92827c92212f423c8a7b11b57
rendengbin <rendengbin_123@163.com>
2016-11-30 22:20:07 +0800

    work

--- 11_27qsort_int_arr.c
+++ 11_27qsort_int_arr.c
@@ -1,22 +1,22 @@
 #include<stdio.h>
 #include<stdlib.h>
-
 int my_cmp(const *a,const *b)
 {
 	return *a-*b;
 }
 
+
 int main()
 {
 	int i=0;
 	int n=0;
-	int *arr;
+	float *arr;
 	arr=(int*)malloc(n*sizeof(int));
 	printf("Please Enter Number:");
 	scanf("%d",&n);
 	for(i=0;i<n;i++)
 	{
-		scanf("%d",(arr+i));
+		scanf("%d",arr+i);
 	}
 	qsort(arr,n,sizeof(int),my_cmp);
 	for(i=0;i<n;i++)
@@ -26,7 +26,3 @@ int main()
 	return 0;
 }
 
-int comp(const void*a,const void*b)
-{
-return *(int*)b-*(int*)a;
-}
